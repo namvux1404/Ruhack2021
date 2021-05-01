0af2e201e2d24b8369606670080212b400a7cfd2
Delvas <ming_d21@hotmail.com>
2021-04-30 21:17:35 -0400

    Update ruhack.js

--- Project/ruhack.js
+++ Project/ruhack.js
@@ -1,14 +1,21 @@
-var a = 1;
-var b = 2;
-var c = 1;
-var d = 2;
-var e = 1;
+import { Textbox } from 'react-inputs-validation';
 
-var sum = function(a,b) {
-    a+b;
-}
-
-
-var lettre = function(a){
-    return a; 
-}
+<Textbox
+  attributesInput={{ // Optional.
+    id: 'Name',
+    name: 'Name',
+    type: 'text',
+    placeholder: 'Place your name here ^-^',
+  }}
+  value={nameRg} // Optional.[String].Default: "".
+  placeholder="Place your name here ^-^" // Optional.[String].Default: "".
+  onChange={(name, e) => {
+    this.setState({ nameRg });
+    console.log(e);
+  }} // Required.[Func].Default: () => {}. Will return the value.
+  onBlur={(e) => {console.log(e)}} // Optional.[Func].Default: none. In order to validate the value on blur, you MUST provide a function, even if it is an empty function. Missing this, the validation on blur will not work.
+  validationOption={{
+    reg: /^d{5}$/, // Optional.[Bool].Default: "" Custom regex.
+    regMsg: 'failed in reg.test(value)' // Optional.[String].Default: "" Custom regex error message.
+  }}
+/>
